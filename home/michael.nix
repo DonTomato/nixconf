5134cfd7edddc874de55b659a289c7dcc8d97740
Mikhail Voronov <michael.voronov@gmail.com>
2026-04-06 21:18:22 +0200

    some fixes in sway

--- home/michael.nix
+++ home/michael.nix
@@ -13,7 +13,33 @@
     enable = true;
 
     config = {
+      modifier = "Mod4"; # Win key
+
       terminal = "alacritty";
+
+      keybindings = let
+        mod = "Mod4";
+      in {
+        "${mod}+Return" = "exec alacritty";
+
+        # переключение workspace
+        "${mod}+1" = "workspace number 1";
+        "${mod}+2" = "workspace number 2";
+        "${mod}+3" = "workspace number 3";
+        "${mod}+4" = "workspace number 4";
+        "${mod}+5" = "workspace number 5";
+
+        # перемещение окон
+        "${mod}+Shift+1" = "move container to workspace number 1";
+        "${mod}+Shift+2" = "move container to workspace number 2";
+        "${mod}+Shift+3" = "move container to workspace number 3";
+        "${mod}+Shift+4" = "move container to workspace number 4";
+        "${mod}+Shift+5" = "move container to workspace number 5";
+
+        # базовое
+        "${mod}+q" = "kill";
+        "${mod}+d" = "exec dmenu_run";
+      };
 
       startup = [
         { command = "waybar"; }
