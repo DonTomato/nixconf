7183f8b204b3ca552660ecb27a8398d65a291171
Mikhail Voronov <michael.voronov@gmail.com>
2026-04-26 18:34:05 +0200

    Add gtk4 theme configuration to home-manager settings

--- home/michael.nix
+++ home/michael.nix
@@ -37,6 +37,7 @@ in {
     };
     gtk3.extraConfig.gtk-application-prefer-dark-theme = 1;
     gtk4.extraConfig.gtk-application-prefer-dark-theme = 1;
+    gtk4.theme = config.gtk.theme;
   };
 
   qt = {
